eaa7c92519b94f88373f391ca8d32cb20105685e
Victoria B <viktorija.bourassa@gmail.com>
2018-12-18 02:10:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -18,9 +18,8 @@ def hopper
 
 
 end
-programmer_hash["alan_kay"]["known_for"]
-def alan_kay_is_known_for
 
+def alan_kay_is_known_for
 
 	programmer_hash =
  		{
@@ -37,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+		 return programmer_hash["alan_kay"]["known_for"]
 end
 
 def dennis_ritchies_language
