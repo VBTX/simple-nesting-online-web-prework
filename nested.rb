a408a34846fae170f99104c394afaabc6e360669
Victoria B <viktorija.bourassa@gmail.com>
2018-12-18 02:18:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,12 +64,10 @@ end
 
 def adding_matz
 
-
 	programmer_hash =
  		{
-
-			:yukihiro_matsumoto => {
-		   :known_for => "Ruby",
+				:yukihiro_matsumoto => {
+		   	:known_for => "Ruby",
 		    :languages => ["LISP", "C"]
 		 }
         :grace_hopper => {
