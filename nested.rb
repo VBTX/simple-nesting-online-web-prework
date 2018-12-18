a31b1017ed606749b56500f6159f7eaeb7c58cd2
Victoria B <viktorija.bourassa@gmail.com>
2018-12-18 02:11:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-programmer_hash["alan_kay"]["known_for"]
+
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -126,3 +126,4 @@ def adding_to_dennis
      }
 
 end
+programmer_hash["alan_kay"]["known_for"]
